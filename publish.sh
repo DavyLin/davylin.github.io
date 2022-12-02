9315296257acd0fe4a3ed83407bd0edeaa6eddce
kinhung <linjxljx@gmail.com>
2022-12-02 11:30:56 +0800

    update index

--- publish.sh
+++ publish.sh
@@ -8,12 +8,12 @@ echo "Server running on localhost:8000 -- press ^C to exit."
 echo "##############################################################################"
 echo ""
 
-python3 -m http.server --directory ./org/post/
+#python3 -m http.server --directory ./org/
 
-echo -n "OK to publish site? [y/N]: "
+#echo -n "OK to publish site? [y/N]: "
 
-read YN
+#read YN
 
-if [ "$YN" = "y" -o "$YN" = "Y" ] ; then
-    rsync -avz --delete --delete-after --progress ./org/post/ blog.kinhung.me:/var/www/kinhungm/
-fi
+#if [ "$YN" = "y" -o "$YN" = "Y" ] ; then
+ #   rsync -avz --delete --delete-after --progress ./org/ kinhung.me:/var/www/kinhungm/
+#fi
